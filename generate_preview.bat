04d05b8205a5537b2dba0c8d26089a7c3bbcbebf
Boris Fritscher <boris@fritscher.ch>
2018-07-05 11:22:38 +0200

    fix generate preview

--- generate_preview.bat
+++ generate_preview.bat
@@ -1,2 +1,2 @@
 cd preview
-for /F "tokens=*" %%A in (urls.txt) do pageres --delay 2 320x480 1024x768 1600x1200 %%A
+for /F "tokens=*" %%A in (urls.txt) do pageres --delay 10 320x480 1024x768 1600x1200 %%A
