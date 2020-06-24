e198be2f24c9ea207676ed34f1211d07744b19ec
Boris Fritscher <boris@fritscher.ch>
2020-06-24 12:21:33 +0200

    fix preview generation

--- generate_preview.bat
+++ generate_preview.bat
@@ -7,8 +7,8 @@ goto END
 set url=%1
 set url=%url:https://=%
 set url=%url:/=!%
-call capture-website %1 --full-page --emulate-device="Nexus 6" --overwrite %url%-phone.png
-call capture-website %1 --full-page --emulate-device="iPad landscape" --overwrite %url%-tablet.png
-call capture-website %1 --full-page --width 1600 --height 1200 --overwrite %url%-desktop.png
+call capture-website %1 --full-page --emulate-device="Nexus 6" --overwrite --output %url%-phone.png
+call capture-website %1 --full-page --emulate-device="iPad landscape" --overwrite --output %url%-tablet.png
+call capture-website %1 --full-page --width 1600 --height 1200 --overwrite --output %url%-desktop.png
 
 :END
